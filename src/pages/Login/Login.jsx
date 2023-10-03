2aeb1cbb3883030d5fe76ef4c132534843773cd1
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-10-03 23:13:32 +0600

    changing directory

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2 className="text-4xl">this is log in page</h2>
+        </div>
+    );
+};
+
+export default Login;
